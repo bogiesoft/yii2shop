a83c71bfe71f26d4e2fdd95a542bbb6a5a4166c2
JinXinAuzn <690550322@qq.com>
2017-06-08 16:24:42 +0800

    配置中文以及伪静态

--- backend/config/main.php
+++ backend/config/main.php
@@ -10,8 +10,8 @@ return [
     'id' => 'app-backend',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
-    'bootstrap' => ['log'],
 	'language'=>'zh-CN',
+    'bootstrap' => ['log'],
     'modules' => [],
     'components' => [
         'request' => [
